5396264e807c918f47baab27606f2646085a85a8
dragg54 <ajibolasadiq@yahoo.com>
2022-09-06 23:50:22 +0100

    added image upload

--- controllers/CarController.js
+++ controllers/CarController.js
@@ -2,21 +2,22 @@ const { CarDb } = require("../models/CarModel")
 
 exports.create = (req, res) =>{
     cars = new CarDb({
-        model: req.body.model,
-        price: req.body.price,
-        color: req.body.color,
-        consumption: req.body.consumption,
-        speed: req.body.speed, 
-        overclocking: req.body.overclocking,
-        battery: req.body.battery,
-        modelYear: req.body.year,
-        engine: req.body.engine,
-        numberOfSeats: req.body.seats,
-        transmission: req.body.transmission
-    })
+      model: req.body.model,
+      price: req.body.price,
+      color: req.body.color,
+      consumption: req.body.consumption,
+      speed: req.body.speed,
+      overclocking: req.body.overclocking,
+      battery: req.body.battery,
+      modelYear: req.body.year,
+      engine: req.body.engine,
+      numberOfSeats: req.body.seats,
+      transmission: req.body.transmission,
+      carImage: req.file.path,
+    });
     cars.save(cars)
     .then(response=>{
-        res.status(302).redirect("http://localhost:3000/cars")
+        res.status(302).redirect("index")
     })
     .catch(err=>{
         console.log(err)
